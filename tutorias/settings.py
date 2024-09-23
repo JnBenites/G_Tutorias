33412571c9fc0b25add9e4ffa3836858dfe2fa3a
Juan B <55644440+JnBenites@users.noreply.github.com>
2024-09-23 11:43:11 -0500

    change settings a mysql

--- tutorias/settings.py
+++ tutorias/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'tutorias.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gtutorias',
+        'USER': 'root',
+        'PASSWORD': 'admin2023',
+        'HOST': 'localhost', 
+        'PORT': '3307', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
